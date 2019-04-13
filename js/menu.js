61ca1a4ad0ab6975d3432ce993ae77ee684e28c6
Xwilarg <xwilarg@yahoo.fr>
2019-04-13 19:17:28 +0200

    Add basic architecture

---
+++ js/menu.js
@@ -0,0 +1,10 @@
+document.getElementById("menu").innerHTML =
+'<div class="sidenav">\
+    <a href="https://www.zirk.eu/index.html">Home</a>\
+    <a href="https://www.zirk.eu/sanara.html">Sanara</a>\
+    <a href="https://www.zirk.eu/boorusharp.html">BooruSharp</a>\
+    <a href="https://www.zirk.eu/nhentaidownloader.html">NHentai Downloader</a>\
+    <a href="https://horizon.zirk.eu">Horizon</a>\
+    <a href="https://www.zirk.eu/lania.html">Lania</a>\
+    <a href="https://www.zirk.eu/gamejam.html">Game Jam</a>\
+</div>'
