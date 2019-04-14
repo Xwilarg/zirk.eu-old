0ba202829b76d6b726baaeecd2927ec2bb29e7ab
Xwilarg <xwilarg@yahoo.fr>
2019-04-14 02:11:37 +0200

    Update menu bar

--- js/menu.js
+++ js/menu.js
@@ -5,6 +5,7 @@ document.getElementById("menu").innerHTML =
     <a href="https://www.zirk.eu/boorusharp.html">BooruSharp</a>\
     <a href="https://www.zirk.eu/nhentaidownloader.html">NHentai Downloader</a>\
     <a href="https://horizon.zirk.eu">Horizon</a>\
+    <a href="https://commits.zirk.eu">Commit Message Analyzer</a>\
     <a href="https://www.zirk.eu/lania.html">Lania</a>\
     <a href="https://www.zirk.eu/gamejam.html">Game Jam</a>\
 </div>'
