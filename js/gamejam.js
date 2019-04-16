30fbe6c65802fe9f2d080d4cf2df51b6007ea019
Xwilarg <xwilarg@yahoo.fr>
2019-04-16 23:52:22 +0200

    Add gamejam buttons

---
+++ js/gamejam.js
@@ -0,0 +1,9 @@
+function updateDescription(name, jamName, jamStart, jamDuration, githubLink, projectLink, postModifications, teamSize) {
+    document.getElementById('sidenavGamejam').innerHTML = '<h3>' + name + '</h3>'
+    + '<br/><br/>' + jamName + '\nTheme: ' + jamTheme + '<br/><br/>'
+    + jamStart + ' (' + jamDuration + ')'
+    + ((githubLink === "None") ? ("") : ('<br/><br/><a href="' + githubLink + '">GitHub</a>'))
+    + ((projectLink === "None") ? ("") : ('<br/><br/><a href="' + projectLink + '">Project page</a>'))
+    + "<br/><hr><br/>Post gamejam modifications:<br/>" + postModifications
+    + "<br/><br/>Team size:<br/>" + teamSize;
+}
