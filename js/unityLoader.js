b68cc935e02c09a5a0527130eb03e1630f28e2ab
Xwilarg <xwilarg@yahoo.fr>
2019-04-18 13:31:19 +0200

    Fix ToFSDfSftPDRRoDwL not loading

--- js/unityLoader.js
+++ js/unityLoader.js
@@ -4,44 +4,72 @@ function setFullscreen() {
     gameInstance.SetFullscreen(1);
 }
 
-var url = new URL(window.location.href);
+const url = new URL(window.location.href);
 switch (url.searchParams.get("jamName")) {
     case "larryTheSheepsMuseumInvasion":
-        gameInstance = UnityLoader.instantiate("gameContainer", "../Unity/LarryTheSheepsMuseumInvasion/LarryTheSheepsMuseumInvasion.json", {onProgress: UnityProgress});
+        $.getScript('../Unity/TemplateData/UnityLoader.js', function()
+        {
+            gameInstance = UnityLoader.instantiate("gameContainer", "../Unity/LarryTheSheepsMuseumInvasion/LarryTheSheepsMuseumInvasion.json", {onProgress: UnityProgress});
+        });
     break;
 
     case "kibouNoImouto":
-        gameInstance = UnityLoader.instantiate("gameContainer", "../Unity/KibouNoImouto/KibouNoImouto.json", {onProgress: UnityProgress});
+        $.getScript('../Unity/TemplateData/UnityLoader.js', function()
+        {
+            gameInstance = UnityLoader.instantiate("gameContainer", "../Unity/KibouNoImouto/KibouNoImouto.json", {onProgress: UnityProgress});
+        });
     break;
 
     case "jovialJudgement":
-        gameInstance = UnityLoader.instantiate("gameContainer", "../Unity/JovialJudgement/ChristmasJam2018WebGL.json", {onProgress: UnityProgress});
+        $.getScript('../Unity/TemplateData/UnityLoader.js', function()
+        {
+            gameInstance = UnityLoader.instantiate("gameContainer", "../Unity/JovialJudgement/ChristmasJam2018WebGL.json", {onProgress: UnityProgress});
+        });
     break;
 
     case "toFSDfSftPDRRoDwL":
-        gameInstance = UnityLoader.instantiate("gameContainer", "../Unity/ToFSDfSftPDRRoDwL/WebGL.json", {onProgress: UnityProgress});
+        $.getScript('../Unity/ToFSDfSftPDRRoDwL/UnityLoader.js', function()
+        {
+            gameInstance = UnityLoader.instantiate("gameContainer", "../Unity/ToFSDfSftPDRRoDwL/WebGL.json", {onProgress: UnityProgress});
+        });
     break;
 
     case "manchaud":
-        gameInstance = UnityLoader.instantiate("gameContainer", "../Unity/Manchaud/TestWebGL.json", {onProgress: UnityProgress});
+        $.getScript('../Unity/TemplateData/UnityLoader.js', function()
+        {
+            gameInstance = UnityLoader.instantiate("gameContainer", "../Unity/Manchaud/TestWebGL.json", {onProgress: UnityProgress});
+        });
     break;
 
     case "powerDown":
-        gameInstance = UnityLoader.instantiate("gameContainer", "../Unity/PowerDown/PowerDownWebGL.json", {onProgress: UnityProgress});
+        $.getScript('../Unity/TemplateData/UnityLoader.js', function()
+        {
+            gameInstance = UnityLoader.instantiate("gameContainer", "../Unity/PowerDown/PowerDownWebGL.json", {onProgress: UnityProgress});
+        });
     break;
 
     case "hadipoRun":
-        gameInstance = UnityLoader.instantiate("gameContainer", "../Unity/HadipoRun/HadipoRunWebGL.json", {onProgress: UnityProgress});
+        $.getScript('../Unity/TemplateData/UnityLoader.js', function()
+        {
+            gameInstance = UnityLoader.instantiate("gameContainer", "../Unity/HadipoRun/HadipoRunWebGL.json", {onProgress: UnityProgress});
+        });
     break;
 
     case "talesOfLayinskia":
-        gameInstance = UnityLoader.instantiate("gameContainer", "../Unity/TalesOfLayinskia/TalesOfLayinskiaWebGL.json", {onProgress: UnityProgress});
+        $.getScript('../Unity/TemplateData/UnityLoader.js', function()
+        {
+            gameInstance = UnityLoader.instantiate("gameContainer", "../Unity/TalesOfLayinskia/TalesOfLayinskiaWebGL.json", {onProgress: UnityProgress});
+        });
     break;
 
     case "vespias":
-        gameInstance = UnityLoader.instantiate("gameContainer", "../Unity/Vespias/VespiasWebGL.json", {onProgress: UnityProgress});
+        $.getScript('../Unity/TemplateData/UnityLoader.js', function()
+        {
+            gameInstance = UnityLoader.instantiate("gameContainer", "../Unity/Vespias/VespiasWebGL.json", {onProgress: UnityProgress});
+        });
     break;
 
     default:
         console.error("Invalid jam name " + url.searchParams.get("jamName"));
+        window.location.href = 'https://zirk.eu/gamejam.html'
 }
