e01c55151bf4103e99d77585569ca3a9bc3e21b3
Xwilarg <xwilarg@yahoo.fr>
2019-05-09 16:32:35 +0200

    Remove useless pages

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,4 @@
 document.getElementById("menu").innerHTML =
 '<div id="sidenav">\
     <a href="index.html">Home</a>\
-    <a href="sanara.html">Sanara</a>\
-    <a href="boorusharp.html">BooruSharp</a>\
-    <a href="nhentaidownloader.html">NHentai Downloader</a>\
-    <a href="horizon.html">Horizon</a>\
-    <a href="commitmessageanalyzer.html">Commit Message Analyzer</a>\
-    <a href="lania.html">Lania</a>\
-    <a href="gamejam.html">Game Jam</a>\
 </div>'
