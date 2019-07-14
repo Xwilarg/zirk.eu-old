83c293bb048c0fbf89ff181f0c5512e99cd93a1d
Xwilarg <xwilarg@yahoo.fr>
2019-07-14 19:36:27 +0200

    Improve slideshow

--- js/mainProjects.js
+++ js/mainProjects.js
@@ -1,8 +1,8 @@
 let currId = 0;
 let projects = [
-    ['<a href="https://sanara.zirk.eu">Sanara</a>', "A multipurpose Discord bot providing games, images and more"],
-    ['<a href="https://zirk.eu/boorusharp.html">BooruSharp</a>', "A C# library to browse Booru websites (Gelbooru, Konachan, etc...) easily"],
-    ['<a href="https://chrome.google.com/webstore/detail/nhentai-downloader/dcpdhacgmnhbfaebkcagkakpcighmeol">NHentaiDownloader</a>', "A chrome extension to download doujinshi from NHentai"]
+    ['<a href="https://sanara.zirk.eu">Sanara</a>', "A multipurpose Discord bot providing games, images and more", "img/Slideshow-Sanara.gif"],
+    ['<a href="https://zirk.eu/boorusharp.html">BooruSharp</a>', "A C# library to browse Booru websites (Gelbooru, Konachan, etc...) easily", "img/Slideshow-BooruSharp.gif"],
+    ['<a href="https://chrome.google.com/webstore/detail/nhentai-downloader/dcpdhacgmnhbfaebkcagkakpcighmeol">NHentaiDownloader</a>', "A chrome extension to download doujinshi from NHentai", ""]
 ];
 
 function nextProject() {
@@ -22,6 +22,7 @@ function previousProject() {
 function displayProject() {
     document.getElementById("slideTitle").innerHTML = projects[currId][0];
     document.getElementById("slideDescription").innerHTML = projects[currId][1];
+    document.getElementById("slideBackground").src = projects[currId][2];
 }
 
 displayProject();
