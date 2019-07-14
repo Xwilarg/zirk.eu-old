e44f1beba1eb7249ce7b268bd6b3b3763e1c9715
Xwilarg <xwilarg@yahoo.fr>
2019-07-14 21:16:12 +0200

    Improve slideshow

--- js/mainProjects.js
+++ js/mainProjects.js
@@ -2,7 +2,7 @@ let currId = 0;
 let projects = [
     ['<a href="https://sanara.zirk.eu">Sanara</a>', "A multipurpose Discord bot providing games, images and more", "img/Slideshow-Sanara.gif"],
     ['<a href="https://zirk.eu/boorusharp.html">BooruSharp</a>', "A C# library to browse Booru websites (Gelbooru, Konachan, etc...) easily", "img/Slideshow-BooruSharp.gif"],
-    ['<a href="https://chrome.google.com/webstore/detail/nhentai-downloader/dcpdhacgmnhbfaebkcagkakpcighmeol">NHentaiDownloader</a>', "A chrome extension to download doujinshi from NHentai", ""]
+    ['<a href="https://chrome.google.com/webstore/detail/nhentai-downloader/dcpdhacgmnhbfaebkcagkakpcighmeol">NHentaiDownloader</a>', "A chrome extension to download doujinshi from NHentai", "img/Slideshow-NHentaiDownloader.gif"]
 ];
 
 function nextProject() {
