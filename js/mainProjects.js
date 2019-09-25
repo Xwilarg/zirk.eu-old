5775d296310ad927183d68acdb1537c2cb71fd7d
Xwilarg <xwilarg@yahoo.fr>
2019-09-25 23:11:06 +0900

    Update project list for NSFW

--- js/mainProjects.js
+++ js/mainProjects.js
@@ -5,7 +5,13 @@ let projects = [
     ['<a href="https://sanara.zirk.eu">Sanara</a>', "A multipurpose Discord bot providing games, images and more", "img/Slideshow-Sanara.gif", "https://github.com/Xwilarg/Sanara"]
 ];
 
-if (!url.searchParams.has("s") || (url.searchParams.has("s") && url.searchParams.get("s") !== "1")) {
+if (url.searchParams.has("s") && url.searchParams.get("s") === "1") {
+    allElems = document.getElementsByClassName("nsfw");
+    for (let i = 0; i < allElems.length; i++)
+        allElems[i].hidden = true;
+}
+else
+{
     projects.push(['<a href="https://github.com/Xwilarg/BooruSharp">BooruSharp</a>', "A C# library to browse Booru websites (Gelbooru, Konachan, etc...) easily", "img/Slideshow-BooruSharp.gif", "https://github.com/Xwilarg/BooruSharp"]);
     projects.push(['<a href="http://nhentaidownloader.zirk.eu">NHentai Downloader</a>', "A chrome extension to download doujinshi from NHentai", "img/Slideshow-NHentaiDownloader.gif", "https://github.com/Xwilarg/NHentaiDownloader"]);
 }
