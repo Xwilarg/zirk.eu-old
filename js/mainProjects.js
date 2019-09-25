7015aac9d9735e8fc5b9a2b760f498499971e69b
Xwilarg <xwilarg@yahoo.fr>
2019-09-25 23:01:08 +0900

    Add safe version

--- js/mainProjects.js
+++ js/mainProjects.js
@@ -1,9 +1,14 @@
+let url = new URL(window.location.href);
+
 let currId = 0;
 let projects = [
-    ['<a href="https://sanara.zirk.eu">Sanara</a>', "A multipurpose Discord bot providing games, images and more", "img/Slideshow-Sanara.gif", "https://github.com/Xwilarg/Sanara"],
-    ['<a href="https://github.com/Xwilarg/BooruSharp">BooruSharp</a>', "A C# library to browse Booru websites (Gelbooru, Konachan, etc...) easily", "img/Slideshow-BooruSharp.gif", "https://github.com/Xwilarg/BooruSharp"],
-    ['<a href="http://nhentaidownloader.zirk.eu">NHentai Downloader</a>', "A chrome extension to download doujinshi from NHentai", "img/Slideshow-NHentaiDownloader.gif", "https://github.com/Xwilarg/NHentaiDownloader"]
+    ['<a href="https://sanara.zirk.eu">Sanara</a>', "A multipurpose Discord bot providing games, images and more", "img/Slideshow-Sanara.gif", "https://github.com/Xwilarg/Sanara"]
 ];
+
+if (!url.searchParams.has("s") || (url.searchParams.has("s") && url.searchParams.get("s") !== "1")) {
+    projects.push(['<a href="https://github.com/Xwilarg/BooruSharp">BooruSharp</a>', "A C# library to browse Booru websites (Gelbooru, Konachan, etc...) easily", "img/Slideshow-BooruSharp.gif", "https://github.com/Xwilarg/BooruSharp"]);
+    projects.push(['<a href="http://nhentaidownloader.zirk.eu">NHentai Downloader</a>', "A chrome extension to download doujinshi from NHentai", "img/Slideshow-NHentaiDownloader.gif", "https://github.com/Xwilarg/NHentaiDownloader"]);
+}
 
 function nextProject() {
     currId++;
