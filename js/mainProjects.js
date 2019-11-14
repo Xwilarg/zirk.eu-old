5524c2a91e15d92e202cde266c42c75422d781ff
Xwilarg <xwilarg@yahoo.fr>
2019-11-14 23:28:48 +0900

    Add main projects

--- js/mainProjects.js
+++ js/mainProjects.js
@@ -1,3 +1,13 @@
 function projectPreview(name) {
 
 }
+
+let xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        response = JSON.parse(this.responseText);
+        document.getElementById("sanaraServers").innerHTML = response.serverCount;
+    }
+}
+xmlhttp.open("GET", "https://api.zirk.eu/bots.php?name=Sanara&small=true", true);
+xmlhttp.send();
