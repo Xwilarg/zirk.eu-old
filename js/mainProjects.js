a83862b725043150463898ac69a829990032d996
Xwilarg <xwilarg@yahoo.fr>
2019-11-17 14:53:12 +0900

    Add badges

--- js/mainProjects.js
+++ js/mainProjects.js
@@ -1,13 +1,3 @@
 function projectPreview(name) {
 
 }
-
-let xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        response = JSON.parse(this.responseText);
-        document.getElementById("sanaraServers").innerHTML = response.serverCount;
-    }
-}
-xmlhttp.open("GET", "https://api.zirk.eu/bots.php?name=Sanara&small=true", true);
-xmlhttp.send();
