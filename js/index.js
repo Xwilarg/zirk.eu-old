991d9362325c4d4b31fb9a2198766c071ef6caf9
Xwilarg <xwilarg@yahoo.fr>
2020-01-29 17:25:19 +0900

    Improve front page

--- js/index.js
+++ js/index.js
@@ -1,20 +1,9 @@
-function resetAll() {
-    document.getElementById("introPresentation").hidden = true;
-    document.getElementById("introCompetences").hidden = true;
-    document.getElementById("introHobbies").hidden = true;
-}
-
-function loadAboutMe() {
-    resetAll();
-    document.getElementById("introPresentation").hidden = false;
-}
-
-function loadCompetences() {
-    resetAll();
-    document.getElementById("introCompetences").hidden = false;
-}
-
-function loadHobbies() {
-    resetAll();
-    document.getElementById("introHobbies").hidden = false;
+let url = new URL(window.location.href);
+if (url.searchParams.get("s") === "1")
+{
+    let elems = document.getElementsByClassName("nsfw");
+    for (let i = 0; i < elems.length; i++)
+    {
+        elems[i].hidden = true;
+    }
 }
